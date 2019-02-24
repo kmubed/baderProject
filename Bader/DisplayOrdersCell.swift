9eced83434079df5bc52028386da41248b96dd68
Itc <itc@Itcs-MacBook-Pro.local>
2019-02-24 20:44:06 +0300

    commit

--- Bader/DisplayOrdersCell.swift
+++ Bader/DisplayOrdersCell.swift
@@ -19,7 +19,7 @@ class DisplayOrdersCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-    }
+    } 
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
